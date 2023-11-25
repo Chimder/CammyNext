74fe8d04c775b9b1c80fb45029459980641abeed
Chimder <127688661+Chimder@users.noreply.github.com>
2023-11-25 20:13:35 +0200

    base url

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const nextConfig = {
   reactStrictMode: false,
 };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+module.exports = {
+  env: {
+    BASE_URL: "http://localhost:3000",
+  },
+};
